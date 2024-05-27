b0395756acf706df8a22385540be8cc7fe5a7a9d
guilhemtrd <guilhem.trd@gmail.com>
2024-05-27 15:30:26 +0200

    front + petit chat

--- src/app/components/formulaire/formulaire.component.ts
+++ src/app/components/formulaire/formulaire.component.ts
@@ -4,7 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatSelectModule } from '@angular/material/select'
 import { CarToRent, LocationCity } from "../../models/FormRequest"
 import { KeyValuePipe, NgForOf } from "@angular/common"
-
+//Import css
 @Component({
   selector: 'app-formulaire',
   templateUrl: './formulaire.component.html',
